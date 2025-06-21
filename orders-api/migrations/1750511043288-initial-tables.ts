c4bb652ae50117ee7d665ee7bfc6cb3f8c6e2581
Lysender <lysender@darkstar-win>
2025-06-21 21:05:16 +0800

    fix: add initial migrations

---
+++ orders-api/migrations/1750511043288-initial-tables.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialTables1750511043288 implements MigrationInterface {
+    name = 'InitialTables1750511043288'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "order" ("id" character varying NOT NULL, "customerId" integer NOT NULL, "promotionId" character varying NOT NULL, "productId" character varying NOT NULL, "status" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_1031171c13130102495201e3e20" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "promotion" ("id" character varying NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_fab3630e0789a2002f1cadb7d38" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "inventory" ("id" character varying NOT NULL, "quantity" integer NOT NULL, CONSTRAINT "PK_82aa5da437c5bbfb80703b08309" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "product" ("id" character varying NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "product"`);
+        await queryRunner.query(`DROP TABLE "inventory"`);
+        await queryRunner.query(`DROP TABLE "promotion"`);
+        await queryRunner.query(`DROP TABLE "order"`);
+    }
+
+}
